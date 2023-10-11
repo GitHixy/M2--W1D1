8c2583b1e6818328b0b457c8e5c89eafb4cefe14
GitHixy <michael.pecora.92@gmail.com>
2023-10-11 15:18:46 +0200

    JS Starter Pack

---
+++ script.js
@@ -0,0 +1,26 @@
+// Un algoritmo è come una ricetta. 
+// È una serie di passaggi o istruzioni che seguono un ordine specifico per ottenere un risultato desiderato.
+
+// Somma di due numeri, dichiariamo le variabili ''numero1'' e ''numero2''
+let numero1 = 5;
+let numero2 = 3;
+
+// Utilizziamo l'algoritmo per la somma dichiarando la variabile ''somma''
+let somma = numero1 + numero2;
+
+// Stampiamo il risultato nel terminale
+console.log("La somma è: " + somma)
+
+
+// undefined: variabile dichiarata ma senza valore assegnato.
+// null: variabile dichiarata a cui si assegna esplicitamente valore nullo.
+
+// Prova undefined:
+
+let variabileUndefined;
+console.log(variabileUndefined); 
+
+// Prova null:
+
+let variabileNull = null;
+console.log(variabileNull);
